821ac99cb004219aae4471df5d664e910f667169
emilytwines <emdelbridge@gmail.com>
2022-11-02 16:33:02 -0400

    worked on Game class, if statement in Player

--- game.py
+++ game.py
@@ -1,17 +1,55 @@
 
+from human import Human 
+from ai import AI
+from player import Player
+
+human_1 = Human()
+human_2 = Human()
+
 class Game:
 
    def __init__(self):
-    pass
+    self.rules = ""
    
    def run_game(self):
-    pass
+
+      player_number = self.display_welcome()
+      self.play_game(player_number)
+      self.win_game()
 
    def display_welcome(self):
-    pass
-   
-   def play_game(self):
-    pass
+      number_of_players = input('''Welcome to Rock Paper Scissors Spock! How many players would you like to have? (single/multi). 
+      We're playing best out of three, so first to two wins!''')
+      print(f"{self.rules}")
+      return number_of_players
+
+
+   def play_game(self, number_of_players):
+      
+      if number_of_players == "single":
+         self.human.make_choice
+         human_1_wins = self.human_1.wins
+         human_2_wins = self.huamn_2.wins
+
+         while human_1_wins < 2 or human_2_wins < 2:
+            pass
+
+
+
+      elif number_of_players == "multi":
+         self.ai.make_choice
+         human_wins = self.human.wins
+         ai_wins = self.ai.wins
+         
+         while human_wins < 2 or ai_wins < 2:
+            pass
+
+      else: 
+         print("I'm sorry, I didn't understand that message!")
+      
+      self.ai.wins
+
+      
 
    def win_game(self):
-    pass
+      pass
--- player.py
+++ player.py
@@ -3,4 +3,73 @@
 class Player:
 
     def __init__(self):
-        pass
+        self.name = ''
+        self.wins = 0
+
+        #list to display before each play
+        self.moves_list["Rock", "Paper", "Scissors", "Lizard", "Spock"]
+
+        #determines winner of each round
+    def make_choice(self):
+        
+        choice = input("Player 1, please choose a weapon.")
+        print("""
+        
+        
+        
+        
+        
+        
+        
+        """)
+        choice1 = input("Player 2, please choose a weapon.")
+        winner = 0
+        winner_p2 = 0
+
+        if choice == self.move_list[0]:
+            if choice1 == self.move_list[2] or choice1 == self.move_list[3]:
+                winner+=1
+            
+        elif choice == self.move_list[1]:
+            if choice1 == self.move_list[2] or choice1 == self.move_list[4]:
+                winner+= 1
+           
+        elif choice == self.move_list[2]:
+            if choice1 == self.move_list[1] or choice1 == self.move_list[3]:
+                winner+=1
+        
+        elif choice == self.move_list[3]:
+            if choice1 == self.move_list[4] or choice1 == self.move_list[2]:
+                winner+= 1
+        
+        elif choice == self.move_list[4]:
+            if choice1 == self.move_list[2] or choice1 == self.move_list[0]:
+                winner+=1
+
+
+        
+        if choice1 == self.move_list[0]:
+            if choice == self.move_list[2] or choice1 == self.move_list[3]:
+                winner_p2 +=1
+            
+        elif choice1 == self.move_list[1]:
+            if choice == self.move_list[2] or choice1 == self.move_list[4]:
+                winner_p2 +=1
+           
+        elif choice1 == self.move_list[2]:
+            if choice == self.move_list[1] or choice1 == self.move_list[3]:
+                winner_p2 +=1
+        
+        elif choice1 == self.move_list[3]:
+            if choice == self.move_list[4] or choice1 == self.move_list[2]:
+                winner_p2 +=1
+        
+        elif choice1 == self.move_list[4]:
+            if choice == self.move_list[2] or choice1 == self.move_list[0]:
+                winner_p2 +=1
+        
+        
+        else:
+            print("I'm sorry, I didn't understand that message!")
+        
+        return winner, winner_p2
